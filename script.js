ab163cc282728a9df02b749346df5c11dece7cdb
tomfraczek <tomaszfr90@gmail.com>
2019-10-31 12:50:15 +0000

    homepage: display article

--- script.js
+++ script.js
@@ -228,7 +228,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     articleTitleContent.appendChild(articleTitle);
 
-
+    document.body.insertBefore(articleTitleContent, articleContainer);
 
 
   }
