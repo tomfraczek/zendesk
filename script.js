f19d18e5714f17d7e8069522d71c1143fa177c5c
tomfraczek <tomaszfr90@gmail.com>
2019-10-31 15:56:13 +0000

    response again

--- script.js
+++ script.js
@@ -213,7 +213,7 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   const request = new XMLHttpRequest();
-  const url='https://audiopartnership1571922554.zendesk.com/api/v2/help_center/en-us/articles.json';
+  const url='https://audiopartnership1571922554.zendesk.com/api/v2/help_center/articles/search.json?query=footer';
   request.open("GET", url);
   request.responseType = 'json';
   request.send();
