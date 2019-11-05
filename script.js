4987157b9c08a0c6e19443086af03a117df390f9
tomfraczek <tomaszfr90@gmail.com>
2019-11-05 09:35:06 +0000

    api promoted articles promises

--- script.js
+++ script.js
@@ -256,26 +256,26 @@ document.addEventListener('DOMContentLoaded', function() {
 
       const sectionsFiltered = sections.filter(section => section.id = sectionIdsArr);
 
+      const listElementItem = document.querySelector('.list-element--item');
+      listElementItem.addEventListener('load', function () {
+        console.log('1');
 
+        // for (let i = 0; i < listElementItem.length; i++){
+        //   console.log('3');
+        //   console.log(listElementItem[i].dataset.id);
+        //
+        //   // if(listElementItem[i].dataset.id === sectionsFiltered[i]){
+        //   //
+        //   // }
+        // }
+      })
       for (let i = 0; i < sectionsFiltered.length; i++){
         // console.log(sectionsFiltered[i].name);
         // console.log(sectionsFiltered[i].html_url);
 
 
 
-          const listElementItem = document.querySelector('.list-element--item');
-          listElementItem.addEventListener('load', function () {
-            console.log('1');
 
-            // for (let i = 0; i < listElementItem.length; i++){
-            //   console.log('3');
-            //   console.log(listElementItem[i].dataset.id);
-            //
-            //   // if(listElementItem[i].dataset.id === sectionsFiltered[i]){
-            //   //
-            //   // }
-            // }
-          })
       }
 
 
