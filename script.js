7b698c1e915dff80ad70cb831ccfa8a1be37cc76
Tom Fraczek <tom.fraczek@cambridgeaudio.com>
2020-08-13 16:38:10 +0200

    Homepage: categories

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     const categoryButtons = document.querySelectorAll('.action');
+    const innerList = document.querySelectorAll('.inner-list');
 
     for (let i = 0; i <= categoryButtons.length; i++){
         categoryButtons[i].addEventListener('click', (e) => {
+            e.preventDefault();
+            console.log(this);
             const list = e.target.parentElement.nextElementSibling;
             list.classList.toggle('show');
+        })
+    }
+
+    for (let i = 0; i <= innerList.length; i++){
+        categoryButtons[i].addEventListener('click', (e) => {
+            e.preventDefault();
+            console.log(this);
+            console.log(e.target);
+            // const list = e.target.parentElement.nextElementSibling;
+            // list.classList.toggle('show');
         })
     }
     //
