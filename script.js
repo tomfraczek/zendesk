10aa6956e0f8a9df98663f0f392947bf9d70c92d
Tom Fraczek <tom.fraczek@cambridgeaudio.com>
2020-08-17 10:57:07 +0200

    Homepage: categories

--- script.js
+++ script.js
@@ -76,16 +76,17 @@ document.addEventListener("DOMContentLoaded", function() {
                     hiFiDropdown.style.height = valueForSmall + "px";
                 } else {
                     for (let i = 0; i <= submenu.length; i++){
-                        document.querySelector('#preList').style.height = (preList + e.target.parentElement.parentElement.offsetHeight) + 'px';
+
                         if(submenu[i]){
                             if (submenu[i].firstElementChild.classList.contains('hifi-button-active')){
                                 console.log('2.1')
+                                document.querySelector('#preList').style.height = (preList + e.target.parentElement.parentElement.offsetHeight) + 'px';
                                 hiFiDropdown.style.height = (e.target.parentElement.nextElementSibling.scrollHeight + hiFiDefault + submenu[i].nextElementSibling.scrollHeight) + "px";
 
                             }
                         }
                     }
-                    document.querySelector('#preList').style.height = (preList + document.querySelector('#preList').offsetHeight) + 'px;'
+                    document.querySelector('#preList').style.height = (preList + e.target.parentElement.nextElementSibling.offsetHeight) + 'px;'
                     e.target.parentElement.parentElement.parentElement.style.height = (e.target.parentElement.nextElementSibling.scrollHeight + e.target.parentElement.parentElement.parentElement.scrollHeight) + 'px';
                     e.target.parentElement.nextElementSibling.style.height = e.target.parentElement.nextElementSibling.scrollHeight + "px";
                     e.target.classList.add('hifi-button-active');
